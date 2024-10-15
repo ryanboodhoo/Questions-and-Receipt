6beacde7f9c811e829d1c951678747c52b62ae2d
ryanboodhoo <boodho46@gmail.com>
2024-10-15 19:28:44 -0400

    first commit

---
+++ src/main/java/Store.java
@@ -0,0 +1,19 @@
+public class Receipt {
+    public static void main(String[] args) {
+        /*
+No extensions. Keep repos private . DM it to me. Thanks
+Create a method named  printReceipt that  scans for 3 pieces of data : a productName, price and quantity.
+Note: The date should correspond to the date that I run your program NOT a hardcoded date!!!
+Your method's job is to leverage your knowledge of the Scanner class, Strings, String formatting, Date class and any other class or classes to generate a receipt in the format below:
+/*
+
+
+
+
+
+
+
+
+
+    }
+}
